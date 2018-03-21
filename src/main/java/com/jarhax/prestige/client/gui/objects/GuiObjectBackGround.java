570fdcdb4bf626741d2e5d797e00c95e8680f484
Jared <jaredlll08@gmail.com>
2018-03-21 20:44:01 +0200

    Changed up the guibackground

--- src/main/java/com/jarhax/prestige/client/gui/objects/GuiObjectBackGround.java
+++ src/main/java/com/jarhax/prestige/client/gui/objects/GuiObjectBackGround.java
@@ -3,6 +3,7 @@ package com.jarhax.prestige.client.gui.objects;
 import com.jarhax.prestige.client.gui.GuiPrestige;
 import com.jarhax.prestige.client.utils.RenderUtils;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 
 public class GuiObjectBackGround extends GuiObject {
@@ -15,6 +16,7 @@ public class GuiObjectBackGround extends GuiObject {
 
     private float offsetAltX;
     private float offsetAltY;
+    
 
     public GuiObjectBackGround (GuiPrestige parent, int x, int y, int width, int height) {
 
@@ -25,14 +27,23 @@ public class GuiObjectBackGround extends GuiObject {
 
     @Override
     public void draw (int left, int top, int mouseX, int mouseY, float partialTicks) {
-
         if (this.visible) {
             this.mc.getTextureManager().bindTexture(BACKGROUND);
             RenderUtils.drawTexturedModalRect(this.getX(), this.getY(), this.offsetX, this.offsetY, this.getWidth(), this.getHeight());
+            
+            
+            GlStateManager.scale(2,2,2);
+            GlStateManager.translate(getX()/2, getY()/2,0);
             this.mc.getTextureManager().bindTexture(BACKGROUND_ALT);
-            RenderUtils.drawTexturedModalRect(this.getX(), this.getY(), this.offsetAltX * 2, this.offsetAltY * 2, this.getWidth(), this.getHeight());
-
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_COLOR, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR);
+            RenderUtils.drawTexturedModalRect(0, 0, this.offsetAltX, this.offsetAltY, this.getWidth()/2, this.getHeight()/2);
+            GlStateManager.translate(-getX()/2, -getY()/2,0);
+            GlStateManager.scale(1,1,1);
         }
+    }
+    
+    public float remap(float value, float from1, float to1, float from2, float to2){
+        return from2 + (value - from1) * (to2 - from2) / (to1 - from1);
     }
 
     @Override
@@ -54,8 +65,8 @@ public class GuiObjectBackGround extends GuiObject {
         // offsetY = Math.min(offsetY, 20);
         // offsetY = Math.max(offsetY, -20);
 
-        this.offsetAltX += (this.getParent().getPrevMX() - mouseX) / 2.5f;
-        this.offsetAltY += (this.getParent().getPrevMY() - mouseY) / 2.5f;
+        this.offsetAltX += (this.getParent().getPrevMX() - mouseX) / 2f;
+        this.offsetAltY += (this.getParent().getPrevMY() - mouseY) / 2f;
 
         // offsetAltX = Math.min(offsetX, 60);
         // offsetAltX = Math.max(offsetX, -60);
